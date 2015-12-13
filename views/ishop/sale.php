4a091baa1e47c19131f6527a5a7239a448b6af4a
werwolf1000 <werwolf100@yandex.ru>
2015-12-13 22:25:17 +0300

    CHANGE

---
+++ views/ishop/sale.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 13.12.2015
+ * Time: 22:10
+ */
+?>
+
+<div class="catalog-index">
+    <h1>Распродажа</h1>
+
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+    <div class="product-index">
+        <h2><a href="">Sony Xperia S 32Gb (черный)</a></h2>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/sony_phone.jpg" alt="Sony Xperia S 32Gb (черный)"></a>
+
+        <p>Цена : <span>24 900</span></p>
+        <a href=""><img src="<?= VIEW . TEMPLATE ?>image/basket_send.png" alt="Добавить в корзину"></a>
+    </div>
+
+</div>
+
