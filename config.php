17d62bcf483ea81774a1134b8983f878c688d4f9
werwolf1000 <werwolf100@yandex.ru>
2015-12-13 18:49:14 +0300

    конфигурационный файл

--- config.php
+++ config.php
@@ -5,3 +5,17 @@
  * Date: 13.12.2015
  * Time: 18:32
  */
+//Адрес сайта
+define('PATH', 'http://marketonkey.local/');
+
+//модель
+define('MODEL', 'model/model.php');
+
+//контроллер
+define('CONTROLLER', 'controller/controller.php');
+
+//вид
+define('VIEW', 'views/');
+
+//активный шаблон
+define('TEMPLATE', 'ishop');
