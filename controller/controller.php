f8af20509cb730a0437b65389036d4bd248450ad
werwolf1000 <werwolf100@yandex.ru>
2015-12-13 21:27:46 +0300

    CHANGE

---
+++ controller/controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 13.12.2015
+ * Time: 21:05
+ */
+defined(ISHOP) or die('Access denied');
+
+//Подключение модели
+require_once MODEL;
+
+//подключение вида
+require_once VIEW . TEMPLATE . 'index.php';
