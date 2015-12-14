5369ee063008ba48a8adc7d197368ddc0e6c22d7
werwolf1000 <werwolf100@yandex.ru>
2015-12-14 23:40:38 +0300

    CHANGE

---
+++ function/function.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 14.12.2015
+ * Time: 22:20
+ */
+defined(ISHOP) or die('Access denied');
+
+/* Распечатка массива */
+function print_arr($arr)
+{
+
+    echo '<pre>';
+    print_r($arr);
+    echo '</pre>';
+}
