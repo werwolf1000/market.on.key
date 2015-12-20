0461f3a66cdbaf48c8f91c394f526c59f3e8cdf7
werwolf1000 <werwolf100@yandex.ru>
2015-12-20 21:00:54 +0300

    CHANGE

--- model/model.php
+++ model/model.php
@@ -6,3 +6,21 @@
  * Time: 21:07
  */
 defined(ISHOP) or die('Access denied');
+
+//Получение массива категорий
+
+function catalog()
+{
+    $query = 'SELECT * FROM brands order by parent_id, brand_name';
+    $res = mysql_query($query);
+    //массив категорий
+    $cat = array();
+    while ($row = mysql_fetch_assoc($res)) {
+        if (!$row['parent_id']) {
+            $cat[$row['brand_id']][] = $row['brand_name'];
+        } else {
+            $cat[$row['parent_id']]['sub'][$row['brand_id']] = $row['brand_name'];
+        }
+    }
+    return $cat;
+}
